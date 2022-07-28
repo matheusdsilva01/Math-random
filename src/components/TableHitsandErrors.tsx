3ab4c3f9f11794c61288941c9c3c32d7bad77add
Matheus Silva <ms25022003@gmail.com>
2022-07-28 10:40:37 -0300

    scroll bar fixed in the bottom into table

--- src/components/TableHitsandErrors.tsx
+++ src/components/TableHitsandErrors.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { Calculation } from "../interfaces/Calculation";
 import './TableHitsandErrors.css';
 
@@ -6,24 +7,31 @@ interface TableHitsandErrorsProps {
     errors: Calculation[]
 }
 function TableHitsandErrors({ hits, errors }: TableHitsandErrorsProps) {
+    const tableCorrects = useRef<HTMLTableRowElement>(null);
+    const tableErrors = useRef<HTMLTableRowElement>(null);
+
     return (
         <>
             <table className="container">
                 {/* table hits */}
-                <tr className="table corrects">
-                    <h1><strong>Acertos</strong></h1>
-                    {hits && hits.map((hit, index) => (
-                        <td key={index}>{hit.conta} = {hit.resposta}</td>
-                    ))}
-                </tr>
+                <tbody>
+                    <tr className="table corrects" ref={tableCorrects}>
+                    <td>
+                        <h1><strong>Acertos</strong></h1>
+                    </td>
+                        {hits && hits.map((hit, index) => (
+                            <td key={index}>{hit.conta} = {hit.resposta}</td>
+                        ))}
+                    </tr>
 
-                {/* table errors */}
-                <tr className="table errors">
-                    <h1><strong>Erros</strong></h1>
-                    {errors && errors.map((error, index) => (
-                        <td key={index}>{error.conta} = {error.resposta}</td>
-                    ))}
-                </tr>
+                    {/* table errors */}
+                    <tr className="table errors" ref={tableErrors}>
+                        <td><h1><strong>Erros</strong></h1>                        </td>
+                        {errors && errors.map((error, index) => (
+                            <td key={index}>{error.conta} = {error.resposta}</td>
+                        ))}
+                    </tr>
+                </tbody>
             </table>
         </>
     )
