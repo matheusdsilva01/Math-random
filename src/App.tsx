66dde073e694a8e0e2a0d1962c54d6ef7a0f08e0
Matheus Silva <ms25022003@gmail.com>
2022-07-29 18:09:06 -0300

    Adding selection of operator math

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,20 @@ import { Calculation } from './interfaces/Calculation';
 function App() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
+  const [operator, setOperator] = useState<string | null>(null);
   const [hits, setHits] = useState<Calculation[]>([]);
   const [errors, setErrors] = useState<Calculation[]>([]);
   const responseCalc = useRef<HTMLInputElement>(null);
+  
   useEffect(() => {
+    if (!operator) {
+      let operations = ['+', '-', '*', '/'];
+      let randomOperator = Math.floor(Math.random() * 4);
+      setOperator(operations[randomOperator]);
+    }
     setNum1(Math.floor(Math.random() * 10 + 1));
     setNum2(Math.floor(Math.random() * 10 + 1));
-  }, [])
+  }, [operator])
 
   const newCount = () => {
     setNum1(Math.floor(Math.random() * 10 + 1));
@@ -24,10 +31,9 @@ function App() {
 
   const calculation = (e: FormEvent) => {
     e.preventDefault()
-    let conta = `${num1} + ${num2}`
+    let conta = `${num1} ${operator} ${num2}`
     let response = responseCalc.current?.value;
-    if (num1 + num2 === Number(response)) {
-
+    if (eval(conta) === Number(response)) {
       addAcert(conta, Number(response))
       newCount();
     } else {
@@ -57,7 +63,18 @@ function App() {
       <ToggleTheme />
       <section>
         <form className='container-form' onSubmit={calculation}>
-          <h1>{num1} + {num2} = <input required type="number" ref={responseCalc} /></h1>
+          <h1>{num1} {operator} {num2} = <input required type="number" step={".01"} ref={responseCalc} /></h1>
+          <select
+            name="operacoes"
+            defaultValue={'null'}
+            onChange={(e) => {setOperator(e.target.value)}}
+          >
+            <option value=''>Aleatório</option>
+            <option value="+">Adição</option>
+            <option value="-">Subtração</option>
+            <option value="*">Multiplicação</option>
+            <option value="/">Divisão</option>
+          </select>
         </form>
       </section>
       <TableHitsandErrors hits={hits} errors={errors} />
